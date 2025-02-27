b6a9863b57bebfc506988019e055730067304e08
JeMing-creater <905120727@qq.com>
2025-02-27 18:47:31 +0800

    first commit

--- gcm.sh
+++ gcm.sh
@@ -1,8 +1,8 @@
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 export OMP_NUM_THREADS=1
-# export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0
 torchrun \
-    --nproc_per_node 8 \
+    --nproc_per_node 1 \
     --master_port 25000 \
     trainer.py jsons/training_config_gcm.json
